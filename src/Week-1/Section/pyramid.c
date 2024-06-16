c00c793b799861edc84b2684110f13f84c27d0c0
Ollie <github@ollie2304.co.uk>
2024-06-16 17:42:55 +0100

    section 1 update

--- src/Week-1/Section/pyramid.c
+++ src/Week-1/Section/pyramid.c
@@ -1,3 +1,19 @@
-//
-// Created by Ollie on 14/06/2024.
-//
+#include <cs50.h>
+#include <stdio.h>
+
+
+void print_row(int length);
+
+int main(void) {
+    int height = get_int("Height:");
+    for (int i = 0; i < height; i++) {
+        print_row((i + 1));
+    }
+}
+
+void print_row(int length) {
+    for (int i = 0; i < length; i++) {
+        printf("#");
+    }
+    printf("\n");
+}
