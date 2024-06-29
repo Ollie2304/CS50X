6aaefedbc97059a70b4f1c1b9c5b738def1d3709
Ollie <boulton.oliver@gmail.com>
2024-06-30 00:03:11 +0100

    Add wk2 section

---
+++ src/Week-2/Section/alphabetical.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void) {
+   string phrase = get_string("Enter a phrase:");
+   int length = strlen(phrase);
+   for (int i = 0; i < length - 1; i++)
+   {
+     if (phrase[i] > phrase[i + 1]) {
+         printf("Not in alphabetical order");
+         return 0;
+     }
+   }
+   printf("These are in alphabetical order.");
+    return 0;
+}
